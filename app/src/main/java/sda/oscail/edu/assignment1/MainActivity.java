e0a4ab21900338b3250b70d9aaba0db1cd93815b
Colin Fleck <fleckc3@mail.dcu.ie>
2019-10-12 22:01:11 +0100

    updated the mainActivity,java to with name and id screen'

--- app/src/main/java/sda/oscail/edu/assignment1/MainActivity.java
+++ app/src/main/java/sda/oscail/edu/assignment1/MainActivity.java
@@ -3,6 +3,8 @@ package sda.oscail.edu.assignment1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //code for show toast method. I got this bit of code from the Assignment page 4
+    public void showToast(View view) {
+        CharSequence text = getResources().getString(R.string.mess_toast);
+
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
     }
 }
